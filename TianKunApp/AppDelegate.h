0584c5c77affa8614baf0a10494215e38058a406
天堃 <tiankun@tiankundeimac.local>
2018-03-19 09:49:41 +0800

    Initial Commit

---
+++ TianKunApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TianKunApp
+//
+//  Created by 天堃 on 2018/3/19.
+//  Copyright © 2018年 天堃. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TianKunApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TianKunApp
+//
+//  Created by 天堃 on 2018/3/19.
+//  Copyright © 2018年 天堃. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
